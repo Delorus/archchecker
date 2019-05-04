ae2b2714a68504913b0245f579b23b9033ae5b61
mshherbakov <sherbakov.maks@gmail.com>
2019-05-04 20:34:39 +0500

    clean code

--- src/test/java/ru/sherb/PlantUMLBuilderTest.java
+++ src/test/java/ru/sherb/PlantUMLBuilderTest.java
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  */
 class PlantUMLBuilderTest {
 
-
     @Test
     void simpleComponentDiagram() {
         var builder = PlantUMLBuilder.newComponentDiagram();
