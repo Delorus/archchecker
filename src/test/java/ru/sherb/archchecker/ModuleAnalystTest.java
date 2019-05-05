a33e9bd0fa66100561fc02454b4ce150de7d2c0b
mshherbakov <sherbakov.maks@gmail.com>
2019-05-05 11:54:27 +0500

    add test to ModuleAnalyst

---
+++ src/test/java/ru/sherb/archchecker/ModuleAnalystTest.java
@@ -0,0 +1,90 @@
+package ru.sherb.archchecker;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author maksim
+ * @since 04.05.19
+ */
+class ModuleAnalystTest {
+
+    @Test
+    @DisplayName("два модуля, первый зависит от второго")
+    void twoModuleFirstDependsToSecond() {
+        // setup
+        var first = createModuleWithClass("first.Class1", "second.Class2");
+        var second = createModuleWithClass("second.Class2");
+
+        // when
+        var analyst = new ModuleAnalyst(List.of(first, second));
+        var modulesStability = analyst.countModulesStability();
+
+        // then
+        assertEquals(1.0, (double) modulesStability.get(first));
+        assertEquals(0.0, (double) modulesStability.get(second));
+    }
+
+    @Test
+    @DisplayName("три модуля, один зависит от двух других")
+    void threeModulesOneDependsToTwo() {
+        // setup
+        var first = createModuleWithClass("first.Class1", "second.Class2", "third.Class3");
+        var second = createModuleWithClass("second.Class2");
+        var third = createModuleWithClass("third.Class3");
+
+        // when
+        var analyst = new ModuleAnalyst(List.of(first, second, third));
+        var modulesStability = analyst.countModulesStability();
+
+        // then
+        assertEquals(1.0, (double) modulesStability.get(first));
+        assertEquals(0.0, (double) modulesStability.get(second));
+        assertEquals(0.0, (double) modulesStability.get(third));
+    }
+
+    @Test
+    @DisplayName("три модуля, которые последовательно зависят друг от друга")
+    void threeModulesSequenceDependency() {
+        // setup
+        var first = createModuleWithClass("first.Class1", "second.Class2");
+        var second = createModuleWithClass("second.Class2", "third.Class3");
+        var third = createModuleWithClass("third.Class3");
+
+        // when
+        var analyst = new ModuleAnalyst(List.of(first, second, third));
+        var modulesStability = analyst.countModulesStability();
+
+        // then
+        assertEquals(1.0, (double) modulesStability.get(first));
+        assertEquals(0.5, (double) modulesStability.get(second));
+        assertEquals(0.0, (double) modulesStability.get(third));
+    }
+
+    private Module createModuleWithClass(String className, String... imports) {
+        var name = new QualifiedName(className);
+
+        var module = new Module(Path.of("test", name.firstPackage()));
+        var builder = Class
+                .builder()
+                .module(module)
+                .name(name.simpleName())
+                .pkg(name.pkg().toString());
+
+        for (String impt : imports) {
+            builder.addImport(impt);
+        }
+
+        var cls1 = builder.build();
+
+        module.setClasses(Collections.singletonList(cls1));
+
+        return module;
+    }
+}
